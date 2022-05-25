0ce432078ff4e0cbdc934b72f68b3eca2f178131
Girik Bajaj <bajajgirik2010@gmail.com>
2022-05-25 15:42:45 +0530

    Added order actions, actionTypes + reducers

---
+++ src/redux/orders/actions.js
@@ -0,0 +1,22 @@
+import { ADD_ORDER_SUCCESS, ORDER_ERROR, DELETE_ORDER_SUCCESS } from "./actionTypes";
+
+export const addOrder = (order) => {
+	return {
+		type: ADD_ORDER_SUCCESS,
+		payload: order
+	};
+}
+
+export const deleteOrder = (orderId) => {
+	return {
+		type: DELETE_ORDER_SUCCESS,
+		payload: orderId	
+	};
+}
+
+export const orderFailed = (msg, err = "") => {
+	return {
+		type: ORDER_ERROR,
+		payload: { msg, err }
+	};
+}
--- src/redux/orders/reducer.js
+++ src/redux/orders/reducer.js
@@ -1,8 +1,34 @@
 import { DUMMY_ORDERS } from "../../constants/dummyData";
-import {} from "./actionTypes";
+import { ADD_ORDER_SUCCESS, ORDER_ERROR, DELETE_ORDER_SUCCESS } from "./actionTypes";
 
-const orderReducer = (state = DUMMY_ORDERS, action) => {
+const initialState = {
+	data: DUMMY_ORDERS,
+	msg: "",
+	err: ""
+}
+
+const orderReducer = (state = initialState, action) => {
 	switch(action.type) {
+		case ADD_ORDER_SUCCESS:
+			return {
+				data: [...state.data, action.payload],
+				msg: "",
+				err: ""
+			};
+
+		case DELETE_ORDER_SUCCESS:
+			return {
+				data: state.data.filter(item => item.id !== action.payload),
+				msg: "",
+				err: ""
+			};
+
+		case ORDER_ERROR:
+			return {
+				...state,
+				msg: action.payload.msg,
+				err: action.payload.err
+			};
 
 		default: return state;
 	}
